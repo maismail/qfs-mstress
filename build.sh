50ae5bd03fc746e426c492b538ab39fa82510d25
Mahmoud Ismail <maism@hops.io>
2015-06-08 18:56:54 +0200

    modify deployment script

---
+++ build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if [ $# -lt 1 ]
+then
+	echo "Usage: $0 <clienttype[hadoop or hops]>"
+	exit
+fi
+
+mvn clean install -P$1
+cd target
+mkdir mstress
+cp ../*.py mstress/
+cp qfs-mstress-1.0-SNAPSHOT-jar-with-dependencies.jar mstress/
+zip -r mstress.zip mstress/
